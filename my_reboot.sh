b0bc475cf63619f0a7d0e9a98f80a1d4c86e4b53
John C Ambrose <jcambro@caen-morty.engin.umich.edu>
2018-05-25 10:22:54 -0400

    More functions added to the reboot script

--- my_reboot.sh
+++ my_reboot.sh
@@ -3,7 +3,7 @@
 #A script to reboot Linux after all users have logged out of their accounts.
 #IDEA: run with nohup &
 
-#### Helper Function
+#### Helper Functions
 function command_line_args {
 	while [ "$1" != "" ]
 	do
@@ -14,17 +14,35 @@ function command_line_args {
 	done
 }
 
+function reset_giveup {
+	#Days go from 1 to 366. Goal is to jump 7 days forward.
+	CURRENT_DATE=$(date +%j)
+	CURRENT_DATE+=6
+	GIVEUP=$(($CURRENT_DATE % 365))
+	#Keeps the value from being 0
+	GIVEUP+=1
+}
+
+function student_or_staff {
+	
+	#Checks for important users
+	if (( $USERCHECK > 999999 ))
+	then
+		echo "There is an importnat user on the system"
+	else
+		echo "There is a student on the system"
+	fi
+}
+
+
 
 #See if any important users are loged on. Sort works from lowest to highest. Looking for high ID numbers.
 USERCHECK=$(id -u | sort -n | tail -n 1)
 NUMBERUSERS=$(id -u | sort -n | wc -l) 
 
-#Set the giveup time to be 7 days later. can tweak this. Days go up to 366 but start at 1. 
-GIVEUP=$(date +%j)
-GIVEUP+=6
-GIVEUP=GIVEUP%365
-#Keeps the value from ever being 0.
-GIVEUP+=1
+#Set the giveup time to be 7 days later 
+GIVEUP=1
+reset_giveup
 
 echo "The number of users logged on: " $NUMBERUSERS
 
@@ -37,26 +55,38 @@ then
 
 	reboot now
 	exit
-
-#Checks for an important user.   NEED TO UPDATE THE NUMBER
-elif (( $USERCHECK > 999999 ))
-then
-	echo "There is someone with credentials logged into this system"
 else
-	echo "A student is logged in"
+	student_or_staff
 fi
 
+PREV_USER=$USERCHECK
+
 #Updates every hour to see if there is a user on. 
-while[ -n "$(who)" ]
+while [ -n "$(who)" ]
 do
-	if[ "$(date +%j)" != "$GIVEUP" ]
+	USERCHECK=$(id -u | sort -n | tail -n 1)
+
+	if [[ "$PREV_USER" -ne "$USERCHECK" ]]
+	then
+		#The same user has to be on for 7 days
+		reset_giveup
+		student_or_staff
+		PREV_USER=$USERCHECK
+	fi
+	
+	
+	if (( "$(date +%j)" != "$GIVEUP" ))
 	then
 		#CHANGE TO 3600 after testing, which is 1 hour.
 		sleep 10
 	else
-		#send an email to alert an admin to kick off the user.
+		#TODO send an email to alert an admin to kick off the user.
 		exit
 	fi
 done
+
+command_line_args
+
+#Handle the arguments
 
 reboot now
